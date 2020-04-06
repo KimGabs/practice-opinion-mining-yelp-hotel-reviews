ed585d6c3fba5104a6fee7a6838d469e53ea36f9
Kim <kim@Macbook-Pro-3.local>
2020-04-06 15:24:30 +0800

    Initial commit

--- Yelp-scrape.py
+++ Yelp-scrape.py
@@ -4,9 +4,13 @@ done in 
 Yelp
 
 """
-
+# Gathering The Data
 import requests
 from bs4 import BeautifulSoup
+
+# # Analysing The Data
+# import pandas as pd
+# import numpy as np
 
 
 def get_page(url):
@@ -20,30 +24,43 @@ def get_page(url):
 
 
 def get_detail_data(soup):
-    # user_review
 
-    # hotel_name
+    # Name of the hotel
     try:
         name = soup.find('h1', attrs={'class': 'lemon--h1__373c0__2ZHSL'}).text
     except:
         name = ''
 
-    # rating
+    # Hotel rating
     try:
         rating = soup.find('div', role='img').get('aria-label')
         rating = rating.strip().split(' ')[0]
     except:
         rating = ''
 
-    # number_of_reviews
+    # number of user reviews
     try:
         num_reviews = soup.find('p', attrs={
             'class': 'lemon--p__373c0__3Qnnj text__373c0__2pB8f text-color--mid__373c0__3G312 text-align--left__373c0__2pnx_ text-size--large__373c0__1568g'})
         num_reviews = num_reviews.text.strip().split(' ')[0]
     except:
         num_reviews = ''
+    # user review content
+    try:
+        usr_review = soup.find('span', lang='en').text
+    except:
+        usr_review = ''
 
-    print(rating)
+    try:
+        divs = soup.findAll('span', lang='en')
+    except:
+        divs = ''
+
+    reviews = []
+    for div in divs:
+        reviews.append(div.text)
+
+    print(reviews[18])
 
 
 def main():
